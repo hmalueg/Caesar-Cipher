4e21f7cea1d04d2c8444fbf70c198528808c1d71
c650 <cbailey@palmertrinity.org>
2015-07-12 12:15:26 -0400

    works with spaces, check it out and learn from it

---
+++ README.md
@@ -0,0 +1,3 @@
+#READ ME FIRST
+The file works for all lowercase strings... You can decide how you want to do it for uppercase as well.
+The script is documented. Just read the notes. 
--- ceasar_cipher.rb
+++ ceasar_cipher.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative "./.support_file.rb" #needs this file... download the entire folder to run caesar_cipher
 $letter_to_num = {'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3, 'e' => 4, 'f' => 5, 'g' => 6, 
 			 'h' => 7, 'i' => 8, 'j' => 9, 'k' => 10, 'l' => 11, 'm' => 12, 'n' =>13, 
@@ -5,23 +6,30 @@ $letter_to_num = {'a' => 0, 'b' => 1, 'c' => 2, 'd' => 3, 'e' => 4, 'f' => 5, 'g
 			 'v' => 21,'w' => 22,'x' => 23,'y' => 24,'z' => 25}
 
 def encrypt(key,phrase)
-	key = key.to_i
-	new_phrase = Array.new
-	phrase = phrase.tr(' ','0')
-    phrase.each_char do |letter|
-    if letter != '0'
-	    number = $letter_to_num[letter]
-	    second_number = number + key
-	    placeholder_hash = $letter_to_num.invert
-	    letter = placeholder_hash[second_number]
-	    new_phrase.push(letter)
+	key = key.to_i #converts the key param into an int to be used in math
+
+ 	unless key < 26 then #checks to see if key exceeds 25, because it would otherwise break the hash
+ 		key -= 26 #makes sure the key is between 0 and 25
+ 	end
+	phrase = phrase.split #splits the phrase into words
+	temp_phrase = Array.new #sets a placeholder array for each word
+	phrase.each do |word| #iterates through each word
+		word = word.split('') #splits each word into array of characters
+		temp_word = Array.new #placeholder array for each character
+			word.each do |letter| #iterates through each word
+				number = $letter_to_num[letter] #basically what you did
+				number += key					#basically what you did
+				temp_hash = $letter_to_num.invert #basically what you did
+				letter = temp_hash[number]		#basically what you did
+				temp_word.push(letter)			#basically what you did
+			end
+		word = temp_word.join('') #saves the manipulated back as word
+		temp_phrase.push(word) #adds the new word to the array of words
 	end
-  end
-  phrase = new_phrase.join
-  phrase = phrase.tr('0',' ')
-  return phrase
+	phrase = temp_phrase.join(' ') #joins array of words to make a string
+	return phrase  #returns the new phrase!
 end
 
-puts encrypt("2", "aa c")
+puts encrypt("3", "ab cd") #this will work for any lowercase string, with spaces.
 
 # print_msg(50) #important line, do not remove.
